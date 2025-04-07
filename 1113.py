fea8211f4afbd05536bf9cad89469f7e2d142aeb
Grazie <alvesgraziella9@gmail.com>
2025-04-07 20:21:41 -0300

    prova

--- 1113.py
+++ 1113.py
@@ -1,32 +1,9 @@
-print("Crescente e Decrescente")
-
-
-# X, Y= list(input())
-# X = int(X)
-# Y = int(Y)
-
-
-while True:
-    xy = list(input().split(" "))
-    break
-
-print(xy)
-for i in xy:
-    print(i)
-    # if xy[0] > xy[1]:
-    #     print("Decrescente")
-    # elif xy[0] < xy[1]:
-    #     print("Crescente")
-    # else:
-    #     break
-
-
 
 while True:
     xy = list(input().split(" "))
     x = int(xy[0])
     y = int(xy[1])
-    if 
+    if x > y:
         print("Decrescente")
     elif x < y:
         print("Crescente")
